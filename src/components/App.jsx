f539995935271cc0d9edcaa8fcd359753499cb58
Denis9622 <deniszinchenko96@gmail.com>
2024-12-30 18:49:15 +0200

    css app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Container from './Container/Container';
 import CatalogPage from '../pages/CatalogPage/CatalogPage';
-import CamperDetailPage from '../pages/CamperDetailPage/CamperDetailPage';
 import SignIn from './S/S';
 import SignUp from '../components/SignUp/SignUp';
 import FavoritesPage from '../pages/FavoritesPage/FavoritesPage';
@@ -11,7 +10,6 @@ function App() {
     <Routes>
       <Route path="/" element={<Container />} />
       <Route path="/catalog" element={<CatalogPage />} />
-      <Route path="/catalog/:id" element={<CamperDetailPage />} />
 
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
