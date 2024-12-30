4ebffe29e004d94f2f402c64a691f4306e833d81
Denis9622 <deniszinchenko96@gmail.com>
2024-12-30 17:46:48 +0200

    1243w45

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { getCurrentUser, logout } from '../Auth/auth';
 import { NavLink } from 'react-router-dom';
-import SignIn from './../SignIn/SignIn';
+import SignIn from '../S/S';
 import SignUp from '../SignUp/SignUp';
 import styles from './Header.module.css';
 
